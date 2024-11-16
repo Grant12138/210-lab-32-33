18ed27f3cb62f924adfec91af591a53b20ef84ce
Grant Luo <hongzhiluo@Tnargs-Mac.local>
2024-11-15 17:37:46 -0800

    Final push

--- main.cpp
+++ main.cpp
@@ -11,8 +11,12 @@ using namespace std;
 
 const int INITIAL_QUEUE_SIZE = 2;
 
+void print_id(string const& lab_desc);
+
 int main()
 {
+    print_id("Lab 32: Toll Booth");
+
     srand(time(0));
 
     // Initialize the queue with two cars
@@ -26,6 +30,7 @@ int main()
         car.print();
     cout << "\n\n";
 
+    // Simulation loop
     int step = 1;
     while (!tollQueue.empty())
     {
@@ -33,6 +38,7 @@ int main()
 
         int probability = 1 + rand() % 99;
 
+        // 55% probability the Car at the head leaves the queue
         if (probability < 55)
         {
             Car departingCar = tollQueue.front();
@@ -41,7 +47,7 @@ int main()
             departingCar.print();
             cout << "\n";
         }
-        else
+        else // 45% probability a new Car joins at the back of the queue
         {
             tollQueue.push_back(Car());
             cout << "New car joined the queue: ";
@@ -50,6 +56,7 @@ int main()
             cout << "\n";
         }
 
+        // Display the current queue
         if (!tollQueue.empty())
         {
             cout << "Current queue:\n";
@@ -64,5 +71,15 @@ int main()
         step++;
     }
 
+    cout << "\n";
     return 0;
+}
+
+void print_id(string const& lab_desc)
+{
+    cout << "\nCOMSC210 | Grant Luo | " << lab_desc << "\n";
+    cout << "Editor: CLion\n";
+    cout << "Compiler: Apple clang version 16.0.0\n";
+    cout << "File: " << __FILE__ << "\n";
+    cout << "Compiled: " << __DATE__ << " at " << __TIME__ << "\n\n";
 }
